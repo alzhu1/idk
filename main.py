86e0e39cd8c2ecc6d55f178dd04c9b1965d1dcc3
Erick Alvarez <erick1998alvarez@gmail.com>
2016-07-25 12:14:33 -0700

    added all pages

--- main.py
+++ main.py
@@ -10,15 +10,37 @@ class Search(ndb.Model):
     location1 = ndb.StringProperty()
     keyword = ndb.StringProperty()
 
-class Result(ndb.Model):
+class Results(ndb.Model):
     eventname = ndb.StringProperty()
     location2 = ndb.StringProperty()
     time = ndb.StringProperty()
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        template = jinja_environment.get_template('home.html')
+        self.response.write(template.render())
+
+class ResultsHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('results.html')
+        self.response.write(template.render())
+        self.response.write('These are the results')
+
+class SpecificsHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('specifics.html')
+        self.response.write(template.render())
+        self.response.write('These are the specifics')
+
+class NopeHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('nope.html')
+        self.response.write(template.render())
+        self.response.write('You think of something')
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/results' ResultsHandler),
+    ('/specifics' SpecificsHandler),
+    ('/nope' NopeHandler)
 ], debug=True)
