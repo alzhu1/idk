fd15e9d584271898ce342c4f29a4aa56d42f1e5a
Alexander Zhu <er.and.alex@gmail.com>
2016-07-26 22:12:18 -0700

    added eventbrite api

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import webapp2
 import jinja2
 import os
 from google.appengine.ext import ndb
-from google.appengine.api import urlfetch
 from yelp.client import Client
 from yelp.oauth1_authenticator import Oauth1Authenticator
+from google.appengine.api import urlfetch
 import logging
+import json
 
 auth = Oauth1Authenticator(
     consumer_key='LTaCUjkWSPDy9gnmJRLM7g',
@@ -15,6 +16,7 @@ auth = Oauth1Authenticator(
 )
 
 client = Client(auth)
+EVENTBRITE_TOKEN = '6V7MG6DMIX6P4FWU5GJW'
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
@@ -70,11 +72,16 @@ class ResultsHandler(webapp2.RequestHandler):
             'radius_filter': 8046,
             'sort': 1
         }
+        EVENTBRITE_URL = 'https://www.eventbriteapi.com/v3/events/search/?token={}&q={}&location.address={}'.format(EVENTBRITE_TOKEN,keywords,location)
+        eventbrite_response = urlfetch.fetch(EVENTBRITE_URL)
+        events = json.loads(eventbrite_response.content)
+
         foods = client.search(location, **food_params) #dropdown menu or search?
-        events = client.search(location, **event_params)
+        yelp_events = client.search(location, **event_params)
         template_vals = {
             'foods': foods,
-            'events': events
+            'yelp_events': yelp_events,
+            'events': events['events']
         }
         logging.info(type(foods)) #REMOVE LATER
         template = jinja_environment.get_template('results.html')
