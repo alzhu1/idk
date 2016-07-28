85c06be64e85a94f1bab9f9a01ce32d0350f6d06
Hariklia Frangos <harikliaf@gmail.com>
2016-07-28 11:56:21 -0700

    messing with results page layout of eentbrite

--- scripts/background.js
+++ scripts/background.js
@@ -1,3 +1,11 @@
 var images = ['balls.jpg', 'bridge.jpg', 'concert.jpg', 'jellyfish.jpg','museum.jpg', 'soccer.jpg', 'nightlife.jpg', 'stars.jpg'];
 
 $('body').css({'background-image': 'url(images/' + images[Math.floor(Math.random() * images.length)] + ')'});
+
+
+var table = document.getElementById("eventbriteTable");
+
+// 1. get to right container
+// 2. calculate how many events you have
+// 3. use that to find dimnesions of table
+// 4. go through that list and put them in the table
