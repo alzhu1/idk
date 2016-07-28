75f50eacf5d2fd7eb69ca5ffd65f9ebbf87b2ce8
Erick Alvarez <erick1998alvarez@gmail.com>
2016-07-28 11:16:53 -0700

    pictures

--- scripts/background.js
+++ scripts/background.js
@@ -1,3 +1,3 @@
-var images = ['balls.jpg', 'bridge.jpg', 'concert.jpg', 'jellyfish.jpg','museum.jpg', 'soccer.jpg', 'nightlife.jpg'];
+var images = ['balls.jpg', 'bridge.jpg', 'concert.jpg', 'jellyfish.jpg','museum.jpg', 'soccer.jpg', 'nightlife.jpg', 'ferriswheel.jpg', 'kelp.jpg', 'hotdog.jpg'];
 
 $('body').css({'background-image': 'url(images/' + images[Math.floor(Math.random() * images.length)] + ')'});
